2a0231f43ad1928ecefcb238495c68994770645c
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-08-16 17:40:05 +0200

    Call initializeWallet method on startup

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import App from '~/components/App';
 import Routes from '~/routes';
 import locales from '../locales';
 import store from '~/configureStore';
+import { initializeWallet } from '~/store/wallet/actions';
+
+store.dispatch(initializeWallet());
 
 const Root = () => (
   <Provider store={store}>
