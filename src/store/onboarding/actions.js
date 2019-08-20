5206fc47300e18f09513d1f432f5a6556884246c
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-08-20 19:02:10 +0200

    Check if restored account does have a safe already

--- src/store/onboarding/actions.js
+++ src/store/onboarding/actions.js
@@ -1,8 +1,16 @@
-import notify from '~/store/notifications/actions';
+import { NOTIFY } from '~/store/notifications/actions';
 import { checkAppState } from '~/store/app/actions';
 import { deployNewSafe, initializeSafeWithNonce } from '~/store/safe/actions';
 import { registerUser } from '~/services/core';
 import { restoreWallet } from '~/store/wallet/actions';
+
+function welcomeUser() {
+  return {
+    [NOTIFY]: {
+      text: 'Welcome!', // @TODO: Use i18n
+    },
+  };
+}
 
 export function checkOnboardingState() {
   return async (dispatch, getState) => {
@@ -24,18 +32,21 @@ export function createNewAccount(username) {
     });
 
     await dispatch(initializeSafeWithNonce());
-
-    dispatch(
-      notify({
-        text: 'Success!', // @TODO: Use i18n
-      }),
-    );
+    dispatch(welcomeUser());
   };
 }
 
 export function restoreAccount(seedPhrase) {
-  return async dispatch => {
+  return async (dispatch, getState) => {
     await dispatch(restoreWallet(seedPhrase));
     await dispatch(checkAppState());
+
+    const { safe } = getState();
+
+    if (!safe.address) {
+      // Restored wallet does not have deployed Safe yet
+      await dispatch(initializeSafeWithNonce());
+      dispatch(welcomeUser());
+    }
   };
 }
