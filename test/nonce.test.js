5f3a1b82cd167de34774def73a1e7b333f29a0c2
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-08-26 14:39:24 +0200

    Add tests for nonce and storage service

---
+++ test/nonce.test.js
@@ -0,0 +1,30 @@
+import {
+  generateNonce,
+  getNonce,
+  hasNonce,
+  removeNonce,
+  setNonce,
+} from '~/services/nonce';
+
+describe('Nonce service', () => {
+  it('should generate a different nonce every time', () => {
+    const firstNonce = generateNonce();
+    const secondNonce = generateNonce();
+
+    expect(firstNonce).not.toEqual(secondNonce);
+  });
+
+  it('should correctly store it and tell us if it exists', () => {
+    expect(hasNonce()).toBe(false);
+
+    const nonce = generateNonce();
+    setNonce(nonce);
+
+    expect(parseInt(getNonce(), 10)).toBe(nonce);
+    expect(hasNonce()).toBe(true);
+
+    removeNonce();
+
+    expect(hasNonce()).toBe(false);
+  });
+});
