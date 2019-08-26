46ef2cf90cc16165e808f3c137cb67763dee3ded
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-08-26 13:50:08 +0200

    Remove deprecated Jest setting

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/config/'],
 
-  // Setup react-testing-library
-  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
-
   // Resolve modules same as Webpack
   moduleNameMapper: {
     '^~(.*)$': '<rootDir>/src$1',
