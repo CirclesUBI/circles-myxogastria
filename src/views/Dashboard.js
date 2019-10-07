04f9218e6f96ade3a7098d58a3f186e1afe4d487
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-10-07 16:34:09 +0200

    Implement welcome view in Dashboard during onboarding

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -5,6 +6,7 @@ import { useSelector } from 'react-redux';
 import ActionButton from '~/components/ActionButton';
 import BalanceDisplay from '~/components/BalanceDisplay';
 import Button from '~/components/Button';
+import Footer from '~/components/Footer';
 import Header from '~/components/Header';
 import QRCode from '~/components/QRCode';
 import TrustHealthDisplay from '~/components/TrustHealthDisplay';
@@ -30,6 +32,40 @@ const Dashboard = () => {
         </Link>
       </Header>
 
+      <DashboardView />
+    </Fragment>
+  );
+};
+
+const DashboardView = (props, context) => {
+  const { isTrusted } = useSelector(state => state.trust);
+  const safe = useSelector(state => state.safe);
+
+  if (!isTrusted) {
+    return (
+      <Fragment>
+        <View>
+          <TrustHealthDisplay />
+          <h1>{context.t('Dashboard.welcomeToCircles')}</h1>
+          <QRCode data={safe.address} width={200} />
+
+          <p>
+            {context.t('Dashboard.trustDescription')}{' '}
+            <a href="#">{context.t('Dashboard.learnMore')}</a>
+          </p>
+        </View>
+
+        <Footer>
+          <Link to="/settings/share">
+            <Button>{context.t('Dashboard.share')}</Button>
+          </Link>
+        </Footer>
+      </Fragment>
+    );
+  }
+
+  return (
+    <Fragment>
       <View>
         <TrustHealthDisplay />
         <TrustNetwork />
@@ -38,6 +74,10 @@ const Dashboard = () => {
       <ActionButton />
     </Fragment>
   );
+};
+
+DashboardView.contextTypes = {
+  t: PropTypes.func.isRequired,
 };
 
 export default Dashboard;
