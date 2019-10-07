546e53f40495e6acb1beb30981c38977d536dbf2
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-10-07 11:20:52 +0200

    Add fallack for QRCodeScanner when only image upload is possible

--- src/components/QRCodeScanner.js
+++ src/components/QRCodeScanner.js
@@ -1,22 +1,89 @@
 import PropTypes from 'prop-types';
 import QrScanner from 'qr-scanner';
 import QrScannerWorkerPath from '!!file-loader!qr-scanner/qr-scanner-worker.min.js';
-import React, { useEffect, createRef } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
+import { useDispatch } from 'react-redux';
+
+import notify, { NotificationsTypes } from '~/store/notifications/actions';
 
 QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
 const QRCodeScanner = props => {
-  const ref = createRef();
+  const dispatch = useDispatch();
 
-  const initializeScanner = () => {
-    new QrScanner(ref.current, result => {
-      props.onSuccess(result);
-    });
+  const [isCameraAvailable, setIsCameraAvailable] = useState(true);
+  const [isOnlyUpload, setIsOnlyUpload] = useState(false);
+
+  const refVideo = createRef();
+  const refInput = createRef();
+
+  const onImageSelected = event => {
+    const image = event.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    QrScanner.scanImage(image)
+      .then(result => {
+        props.onSuccess(result);
+      })
+      .catch(() => {
+        dispatch(
+          notify({
+            text: 'Could not find QR code', // @TODO
+            type: NotificationsTypes.WARNING,
+          }),
+        );
+      });
   };
 
-  useEffect(initializeScanner, []);
+  const initialize = () => {
+    let scanner;
 
-  return <video ref={ref} />;
+    QrScanner.hasCamera().then(cameraResult => {
+      setIsCameraAvailable(cameraResult);
+
+      if (!cameraResult) {
+        return;
+      }
+
+      try {
+        scanner = new QrScanner(refVideo.current, result => {
+          props.onSuccess(result);
+        });
+
+        scanner.start();
+      } catch {
+        // .. fall back on manual upload option
+        setIsOnlyUpload(true);
+      }
+    });
+
+    return () => {
+      if (!scanner) {
+        return;
+      }
+
+      scanner.destroy();
+    };
+  };
+
+  useEffect(initialize, []);
+
+  if (!isCameraAvailable || isOnlyUpload) {
+    return (
+      <input
+        accept="image/*"
+        capture="camera"
+        ref={refInput}
+        type="file"
+        onChange={onImageSelected}
+      />
+    );
+  }
+
+  return <video ref={refVideo} />;
 };
 
 QRCodeScanner.propTypes = {
