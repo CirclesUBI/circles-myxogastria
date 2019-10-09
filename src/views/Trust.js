ea4d0cf60c066da1195692fc435d4b4b79432011
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-10-09 19:43:26 +0200

    Add SafeFinderView to trust view

--- src/views/Trust.js
+++ src/views/Trust.js
@@ -1,15 +1,28 @@
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import BackButton from '~/components/BackButton';
 import Header from '~/components/Header';
+import SafeFinderView from '~/components/SafeFinderView';
 
 const Trust = () => {
+  const [safeAddress, setSafeAddress] = useState('');
+
+  const onSelect = address => {
+    setSafeAddress(address);
+  };
+
+  if (safeAddress) {
+    return <Redirect to={`/trust/${safeAddress}`} />;
+  }
   return (
     <Fragment>
       <Header>
         <BackButton to="/" />
       </Header>
+
+      <SafeFinderView onSelect={onSelect} />
     </Fragment>
   );
 };
