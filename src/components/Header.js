bdb90758287313a7143da24262b5235efdc7b8a8
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-10-11 16:45:00 +0200

    Implement basic HeaderStyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = props => {
-  return <header>{props.children}</header>;
+  return <HeaderStyle>{props.children}</HeaderStyle>;
 };
 
 Header.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+const HeaderStyle = styled.header`
+  display: flex;
+
+  height: 5rem;
+
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export default Header;
