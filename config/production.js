a4af2f20bdd6647ec4a1f542ea028499cc2a1fa5
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-11-18 18:19:54 +0100

    Use env vars for production setups

--- config/production.js
+++ config/production.js
@@ -1,24 +1,16 @@
-module.exports = {
-  // Base path
-  BASE_PATH: '', // @TODO
-
-  // Build environment
-  NODE_ENV: 'production',
-
-  // Blockchain API URL
-  ETHEREUM_NODE_ENDPOINT: '', // @TODO
-
-  // Service Endpoints
-  RELAY_SERVICE_ENDPOINT: '', // @TODO
-  USERNAME_SERVICE_ENDPOINT: '', // @TODO
-  GRAPH_NODE_ENDPOINT: '', // @TODO
-
-  // Smart Contract addresses
-  SAFE_ADDRESS: '', // @TODO
-  PROXY_FACTORY_ADDRESS: '', // @TODO
-  HUB_ADDRESS: '', // @TODO
-  SAFE_FUNDER_ADDRESS: '', // @TODO
-
-  // Graph node
-  SUBGRAPH_NAME: 'CirclesUBI/circles-subgraph',
-};
+module.exports = [
+  'BASE_PATH',
+  'NODE_ENV',
+  'ETHEREUM_NODE_ENDPOINT',
+  'RELAY_SERVICE_ENDPOINT',
+  'USERNAME_SERVICE_ENDPOINT',
+  'GRAPH_NODE_ENDPOINT',
+  'SAFE_ADDRESS',
+  'PROXY_FACTORY_ADDRESS',
+  'HUB_ADDRESS',
+  'SAFE_FUNDER_ADDRESS',
+  'SUBGRAPH_NAME',
+].reduce((acc, key) => {
+  acc[key] = process.env[key];
+  return acc;
+}, {});
