57f272fbd1bd798a6c90b398220bf7814b8f01c2
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-12-18 18:44:31 +0100

    Make update rate slower when in development env

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import notify, { NotificationsTypes } from '~/store/notifications/actions';
 import styles from '~/styles/variables';
 import { checkAppState, initializeApp } from '~/store/app/actions';
 
-const APP_CHECK_FRQUENCY = 1000 * 4;
+const APP_CHECK_FRQUENCY = 1000 * (process.env.ENV === 'production' ? 4 : 10);
 
 const App = (props, context) => {
   const app = useSelector(state => state.app);
