1c322f6474e0e38049efcf10edfca8915624b727
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-12-18 20:32:58 +0100

    Embed sentry for debugging

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import core from '~/services/core';
+import { captureException } from '~/services/sentry';
 
 const { TransferError, RequestError, CoreError } = core.errors;
 
@@ -10,10 +11,15 @@ export default function logError(error) {
 
   if (error instanceof RequestError) {
     console.log(error.request);
+    captureException(error, error.request);
   } else if (error instanceof TransferError) {
     console.log(error.transfer);
+    captureException(error, error.transfer);
   } else if (error instanceof CoreError) {
     console.log(error.code);
+    captureException(error);
+  } else {
+    captureException(error);
   }
 
   console.groupEnd();
