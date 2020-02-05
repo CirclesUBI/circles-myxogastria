976261f99c7406a46a880cd70dca0f7d854ae1cb
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-02-05 22:45:42 +0100

    Fix QR code look

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 import styles from '~/styles/variables';
 
-const QR_CODE_SIZE = 250;
+const QR_CODE_SIZE = 230;
 
 const QRCode = props => {
   const ref = createRef();
