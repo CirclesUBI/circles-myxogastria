2bb1324e1726b106ea38eb12085f752f0183673a
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-02-05 23:21:28 +0100

    Check if address is valid when opening profile

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,15 +1,30 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import web3 from '~/services/web3';
+import { Redirect, withRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import Header from '~/components/Header';
 import ButtonHome from '~/components/ButtonHome';
+import Header from '~/components/Header';
 import ProfileBox from '~/components/ProfileBox';
 import View from '~/components/View';
+import notify, { NotificationsTypes } from '~/store/notifications/actions';
 import { BackgroundPurplePlain } from '~/styles/Background';
 
-const Profile = props => {
+const Profile = (props, context) => {
+  const dispatch = useDispatch();
   const { address } = props.match.params;
+
+  if (!web3.utils.checkAddressChecksum(address)) {
+    dispatch(
+      notify({
+        text: context.t('Profile.invalidAddress'),
+        type: NotificationsTypes.WARNING,
+      }),
+    );
+
+    return <Redirect to="/" />;
+  }
 
   return (
     <BackgroundPurplePlain>
@@ -26,6 +41,10 @@ const Profile = props => {
 
 Profile.propTypes = {
   match: PropTypes.object.isRequired,
+};
+
+Profile.contextTypes = {
+  t: PropTypes.func.isRequired,
 };
 
 export default withRouter(Profile);
