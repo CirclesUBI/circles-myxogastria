daf9d279b57ba181607d04b65be54daf3f9f4558
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-09-18 18:49:53 +0200

    Update website links

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,14 @@
-export const ISSUANCE_RATE_MONTH = 240;
-export const NEEDED_TRUST_CONNECTIONS = 3;
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-// @TODO: Update links
-export const ABOUT_URL = 'https://';
-export const EMAIL_URL = 'mailto:';
-export const FACEBOOK_URL = 'https://';
-export const FAQ_URL = 'https://';
+export const ISSUANCE_RATE_MONTH = 240;
+export const NEEDED_TRUST_CONNECTIONS = 3;
+
+export const ABOUT_URL = 'https://joincircles.net';
+export const EMAIL_URL = 'mailto:hello@joincircles.net';
+export const FACEBOOK_URL = 'https://facebook.com/CirclesUBI';
+export const FAQ_URL = 'https://joincircles.net/faq';
 export const MARKETPLACE_URL = 'https://t.me/CirclesUBI';
-export const PRIVACY_LEGAL_URL = 'https://';
-export const SUPPORT_URL = 'https://';
-export const TELEGRAM_URL = 'https://';
-export const TWITTER_URL = 'https://';
+export const PRIVACY_LEGAL_URL = 'https://joincircles.net';
+export const SUPPORT_URL = 'https://joincircles.net/faq';
+export const TELEGRAM_URL = 'https://t.me/CirclesUBI';
+export const TWITTER_URL = 'https://twitter.com/CirclesUBI';
