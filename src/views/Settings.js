1d9b74627a4ded17cf2be4be0fd14c1d4bab608e
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-10-08 18:01:44 +0200

    Add public address to Settings

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,6 +1,13 @@
 import React, { Fragment, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Box, Container, Paper, Typography } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Box,
+  Container,
+  Chip,
+  Grid,
+  Paper,
+  Typography,
+} from '@material-ui/core';
 
 import Button from '~/components/Button';
 import ButtonBack from '~/components/ButtonBack';
@@ -13,6 +20,7 @@ import { burnApp } from '~/store/app/actions';
 
 const Settings = () => {
   const dispatch = useDispatch();
+  const wallet = useSelector((state) => state.wallet);
   const [isConfirmationShown, setIsConfirmationShown] = useState(false);
 
   const handleBurn = () => {
@@ -42,23 +50,37 @@ const Settings = () => {
       <Header>
         <ButtonBack />
         <CenteredHeading>
-          {translate('Settings.headingManageDevices')}
+          {translate('Settings.headingSettings')}
         </CenteredHeading>
       </Header>
       <View>
         <Container maxWidth="sm">
-          <Paper>
-            <Box p={2}>
-              <Typography align="center" variant="h6">
-                {translate('Settings.headingDangerZone')}
-              </Typography>
-              <Box my={2}>
-                <Button fullWidth isDanger onClick={handleConfirmOpen}>
-                  {translate('Settings.buttonBurnWallet')}
-                </Button>
-              </Box>
-            </Box>
-          </Paper>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Paper>
+                <Box p={2} textAlign="center">
+                  <Typography align="center" gutterBottom variant="h6">
+                    {translate('Settings.headingPublicAddress')}
+                  </Typography>
+                  <Chip label={wallet.address} />
+                </Box>
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper>
+                <Box p={2}>
+                  <Typography align="center" variant="h6">
+                    {translate('Settings.headingDangerZone')}
+                  </Typography>
+                  <Box my={2}>
+                    <Button fullWidth isDanger onClick={handleConfirmOpen}>
+                      {translate('Settings.buttonBurnWallet')}
+                    </Button>
+                  </Box>
+                </Box>
+              </Paper>
+            </Grid>
+          </Grid>
           <Box mt={2}>
             <Typography align="center">
               v. {process.env.RELEASE_VERSION} (
