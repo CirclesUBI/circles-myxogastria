3d98a9da4e7464ac541a828a9cd9079dd65d35ba
Emin Durak <emin.tuta.io>
2021-05-18 18:36:03 +0300

    added the cut circle svg image as background finally

--- src/components/OnboardingOrganizationTutorial.js
+++ src/components/OnboardingOrganizationTutorial.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
 import Dialog from '@material-ui/core/Dialog';
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableViews from 'react-swipeable-views';
@@ -7,27 +8,57 @@ import OrgTutorialStep1SVG from '%/images/org-tutorial-step-1.svg';
 import OrgTutorialStep2SVG from '%/images/org-tutorial-step-2.svg';
 import OrgTutorialStep3SVG from '%/images/org-tutorial-step-3.svg';
 
+const CutCircleSVG = () => {
+  return (
+    <svg
+      height="100%"
+      version="1.1"
+      viewBox="0 0 845 988"
+      width="100%"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        id="Page-1"
+        stroke="none"
+        strokeWidth="1"
+      >
+        <path
+          d="M845,238.697556 L845,749.302444 C758.466603,892.36878 601.401235,988 422,988 C243.287681,988 86.740017,893.101835 -1.84741111e-13,750.948499 L0,237.051501 C86.740017,94.898165 243.287681,0 422,0 C601.401235,0 758.466603,95.6312197 845.000006,238.697563 Z"
+          fill="#FFFFFF"
+          id="Combined-Shape"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+const svgStringCutCircle = encodeURIComponent(
+  renderToStaticMarkup(<CutCircleSVG />),
+);
+const dataUriCutCircle = `url("data:image/svg+xml,${svgStringCutCircle}")`;
+
 const useStyles = makeStyles(() => ({
-  dialogWrapper: {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     width: '100%',
     height: '100%',
     background:
       'linear-gradient(180deg, rgba(215,58,83,1) 0%, rgba(251,134,9,1) 100%)',
   },
   slideContainer: {
-    position: 'absolute',
-    width: '130vw',
-    height: '130vw',
-    top: 'calc(50vh - 65vw)',
-    left: '-15vw',
-    padding: '12px 20vw',
-    margin: 'auto',
-    maxWidth: 480,
-    maxHeight: 480,
-    borderRadius: '50%',
-    background: 'white',
-    display: 'flex',
-    justifyContent: 'center',
+    background: dataUriCutCircle,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: '70vh',
+    padding: 12,
+  },
+  slideBody: {
+    padding: 24,
   },
 }));
 
@@ -71,25 +102,26 @@ const OnboardingOrganizationTutorial = () => {
       // onClose={handleTrustClose}
       // onConfirm={handleTrust}
     >
-      <div className={classes.slideContainer}>
+      <Box className={classes.wrapper}>
         <SwipeableViews index={step} onChangeIndex={(index) => setStep(index)}>
           {slides.map((step) => (
-            <div key={step.heading}>
-              <Box
-                alignItems="center"
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                width={300}
-              >
-                {step.image}
-                <Typography variant="h6">{step.heading}</Typography>
-                <Typography variant="body2">{step.body}</Typography>
-              </Box>
-            </div>
+            <Box
+              alignItems="center"
+              className={classes.slideContainer}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              key={step.heading}
+            >
+              {step.image}
+              <Typography variant="h6">{step.heading}</Typography>
+              <Typography className={classes.slideBody} variant="body2">
+                {step.body}
+              </Typography>
+            </Box>
           ))}
         </SwipeableViews>
-      </div>
+      </Box>
     </Dialog>
   );
 };
