a3b6612303e0a870243d70cf1601f5a24bf25202
Emin Durak <emin.tuta.io>
2021-06-08 01:05:50 +0300

    right import for the Link

--- src/components/OnboardingOrganizationTutorial.js
+++ src/components/OnboardingOrganizationTutorial.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { renderToStaticMarkup } from 'react-dom/server';
 import translate from '~/services/locale';
 import Dialog from '@material-ui/core/Dialog';
-import Link from '@material-ui/core/Dialog';
+import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, IconButton, Slide, Typography } from '@material-ui/core';
 import SwipeableViews from 'react-swipeable-views';
