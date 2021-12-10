136f8303370acb934f97d69316eb1964a9093d7f
mikozet <mikozet2@gmail.com>
2021-12-10 16:26:03 +0100

    Remove redundant dependency

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,4 +1,4 @@
-import { IconButton, MobileStepper, useScrollTrigger } from '@material-ui/core';
+import { IconButton, MobileStepper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
