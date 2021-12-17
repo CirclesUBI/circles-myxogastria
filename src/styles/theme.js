c803146a3b72ab21163721c2326d6027cf2a5885
mikozet <mikozet2@gmail.com>
2021-12-17 13:57:09 +0100

    Update gradient color with variables

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -51,7 +51,7 @@ const gradients = {
   info: `linear-gradient(90deg, ${colors.blue} 0%, ${colors.purpleLight} 100%)`,
   success: `linear-gradient(90deg, ${colors.green} 0%, ${colors.blue} 100%)`,
   warning: `linear-gradient(90deg, ${colors.orangeDark} 0%, ${colors.orange} 100%)`,
-  blue: `linear-gradient(180deg, #48B2B7 0%, #06FC9D 100%)`,
+  blue: `linear-gradient(180deg, ${colors.fountainBlue} 0%, ${colors.springGreen} 100%)`,
 };
 
 const components = {
