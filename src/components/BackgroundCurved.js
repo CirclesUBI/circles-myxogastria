75085421df48a27d3aab0225ceae8c6e5c377884
mikozet <mikozet2@gmail.com>
2022-01-07 13:52:40 +0100

    Change wave background color depending on prop and screen size

--- src/components/BackgroundCurved.js
+++ src/components/BackgroundCurved.js
@@ -3,43 +3,61 @@ import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: '50%',
-    zIndex: theme.zIndex.backgroundCurvedWrapper,
-    top: 0,
-    margin: '0 auto',
-    background: theme.custom.gradients.blue,
-  },
-  background: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    overflow: 'hidden',
-    '&::after': {
-      [theme.breakpoints.up('sm')]: {
-        width: 1153,
-        height: 808,
-      },
+const useStyles = makeStyles((theme) => {
+  return {
+    wrapper: {
       position: 'absolute',
-      display: 'block',
-      top: 553,
-      left: '50%',
-      width: 690,
-      height: 950,
-      content: '""',
-      backgroundColor: 'white',
-      borderRadius: '50%',
-      transform: 'translate3d(-50%, -50%, 0)',
+      left: 0,
+      right: 0,
+      bottom: '50%',
+      zIndex: theme.zIndex.backgroundCurvedWrapper,
+      top: 0,
+      margin: '0 auto',
+      background: (props) => {
+        if (
+          typeof window != 'undefined' &&
+          window.innerWidth >= theme.breakpoints.values.sm
+        ) {
+          switch (props.gradient) {
+            case 'turquoise':
+              return theme.custom.gradients.greenBlueDesktop;
+          }
+        } else {
+          switch (props.gradient) {
+            case 'turquoise':
+              return theme.custom.gradients.greenBlue;
+          }
+        }
+      },
     },
-  },
-}));
+
+    background: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      overflow: 'hidden',
+      '&::after': {
+        width: 750,
+        height: 750,
+        position: 'absolute',
+        display: 'block',
+        top: 476,
+        left: '50%',
+        content: '""',
+        backgroundColor: 'white',
+        borderRadius: '50%',
+        transform: 'translate3d(-50%, -50%, 0)',
+        [theme.breakpoints.up('sm')]: {
+          width: 1153,
+          height: 808,
+        },
+      },
+    },
+  };
+});
 
 const BackgroundCurved = (props) => {
-  const classes = useStyles();
+  const classes = useStyles(props);
 
   return (
     <>
@@ -53,6 +71,7 @@ const BackgroundCurved = (props) => {
 
 BackgroundCurved.propTypes = {
   children: PropTypes.node,
+  gradient: PropTypes.string,
 };
 
 export default BackgroundCurved;
