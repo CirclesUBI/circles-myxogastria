0682601cea2e055843a788b41cdde8b16a3c3a25
Louise Linné <linne.louise@gmail.com>
2022-02-03 21:33:52 +0100

    Fix that profile page cannot render due to incorrect empty return

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -323,7 +323,7 @@ const ProfileSendButton = ({ address, deploymentStatus }) => {
   // b) Our Safe is not deployed yet - show disabled button
   // c) The profiles Safe is not deployed yet - show disabled button
   if (safe.currentAccount === address) {
-    return;
+    return null;
   }
 
   const isSendDisabled =
