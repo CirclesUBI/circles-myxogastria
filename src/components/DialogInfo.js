a4d1305048607a32fa54e71be8c51153c1fe581d
mikozet <mikozet2@gmail.com>
2022-02-17 07:45:18 +0100

    Update DialogInfo component with design

--- src/components/DialogInfo.js
+++ src/components/DialogInfo.js
@@ -4,16 +4,36 @@ import {
   DialogTitle,
   Dialog as MuiDialog,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import ButtonClose from '~/components/ButtonClose';
 
+const useStyles = makeStyles((theme) => ({
+  dialogContainer: {
+    '& .MuiDialog-paper': {
+      borderRadius: '10px',
+      boxShadow: theme.custom.shadows.gray,
+      textAlign: 'center',
+    },
+    '& .MuiBackdrop-root': {
+      background: theme.custom.colors.dialogGray,
+    },
+    '& .MuiDialogTitle-root': {
+      paddingTop: '40px',
+    },
+  },
+}));
+
 const DialogInfo = ({ dialogContent, handleClose, id, isOpen, title }) => {
+  const classes = useStyles();
+
   return (
     <MuiDialog
       aria-describedby={`dialog-${id}-text`}
       aria-labelledby={`dialog-${id}-description`}
+      className={classes.dialogContainer}
       open={isOpen}
       onClose={handleClose}
     >
