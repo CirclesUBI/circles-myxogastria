0885ae40b4cd28eb974f0b7ccd94d3fe5f79cd60
mikozet <mikozet2@gmail.com>
2022-02-18 14:41:21 +0100

    Update stepperConfiguration in OnboardingOrganization

--- src/views/OnboardingOrganization.js
+++ src/views/OnboardingOrganization.js
@@ -115,7 +115,7 @@ const OnboardingOrganization = () => {
     OrganizationStepAddMembers,
   ];
 
-  const screenNames = {
+  const stepsScreens = {
     ENTER_EMAIL: 0,
     FUND_YOUR_ORGANIZATION: 1,
     CREATE_YOUR_USERNAME: 2,
@@ -126,15 +126,15 @@ const OnboardingOrganization = () => {
   const stepperConfiguration = [
     {
       stepName: translate('OnboardingOrganization.stepperFirstStep'),
-      activeTillScreen: screenNames.ENTER_EMAIL,
+      activeTillScreen: stepsScreens.ENTER_EMAIL,
     },
     {
       stepName: translate('OnboardingOrganization.stepperSecondStep'),
-      activeTillScreen: screenNames.CREATE_YOUR_USERNAME,
+      activeTillScreen: stepsScreens.CREATE_YOUR_USERNAME,
     },
     {
       stepName: translate('OnboardingOrganization.stepperThirdStep'),
-      activeTillScreen: screenNames.ADD_MEMBERS,
+      activeTillScreen: stepsScreens.ADD_MEMBERS,
     },
   ];
 
@@ -178,12 +178,11 @@ const OnboardingOrganization = () => {
       <BackgroundCurved gradient="orange">
         <OnboardingStepper
           exitPath={DASHBOARD_PATH}
-          isHorizontalStepper={true}
           mb={16}
           stepperConfiguration={stepperConfiguration}
           steps={steps}
           stepsButtons={stepsButtons}
-          todoRemoveFlag={true}
+          stepsScreens={stepsScreens}
           values={values}
           onFinish={onFinish}
           onValuesChange={setValues}
