4037622b2be74ebb9ae58e39d2d5386e8f6aa08a
mikozet <mikozet2@gmail.com>
2022-03-07 10:56:21 +0100

    Correct imports in OnboardingOrganization

--- src/views/OnboardingOrganization.js
+++ src/views/OnboardingOrganization.js
@@ -6,8 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { generatePath } from 'react-router';
 import { Redirect } from 'react-router-dom';
 
-import { DASHBOARD_PATH, PROFILE_PATH } from '~/routes';
-import { ORGANIZATION_PATH } from '~/routes';
+import { DASHBOARD_PATH, ORGANIZATION_PATH, PROFILE_PATH } from '~/routes';
 
 import AvatarUploader from '~/components/AvatarUploader';
 import BackgroundCurved from '~/components/BackgroundCurved';
