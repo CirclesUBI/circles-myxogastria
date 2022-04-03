591fe81601176b0ed784738d33b97f31e95d84ac
mikozet <mikozet2@gmail.com>
2022-04-03 23:53:56 +0200

    Update DialogTrust - change for purple version

--- src/components/DialogTrust.js
+++ src/components/DialogTrust.js
@@ -1,14 +1,32 @@
+import { Box, Typography, makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-import Dialog from '~/components/Dialog';
+import Avatar from '~/components/Avatar';
+import DialogPurple from '~/components/DialogPurple';
+import ExternalLink from '~/components/ExternalLink';
 import { useUserdata } from '~/hooks/username';
 import translate from '~/services/locale';
 import { hideSpinnerOverlay, showSpinnerOverlay } from '~/store/app/actions';
 import notify, { NotificationsTypes } from '~/store/notifications/actions';
 import { trustUser } from '~/store/trust/actions';
+import { FAQ_URL } from '~/utils/constants';
 import logError from '~/utils/debug';
+
+const useStyles = makeStyles((theme) => ({
+  paragraph: {
+    color: theme.custom.colors.grayLightest,
+    fontWeight: 500,
+  },
+  link: {
+    color: theme.custom.colors.grayLightest,
+
+    '& p': {
+      fontWeight: 500,
+    },
+  },
+}));
 
 const DialogTrust = ({
   address,
@@ -18,6 +36,7 @@ const DialogTrust = ({
   onConfirm,
   onError,
 }) => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const { username } = useUserdata(address);
 
@@ -61,17 +80,27 @@ const DialogTrust = ({
   };
 
   return (
-    <Dialog
+    <DialogPurple
       cancelLabel={translate('DialogTrust.dialogTrustCancel')}
       confirmLabel={translate('DialogTrust.dialogTrustConfirm')}
-      externalPath="#"
       id="trust"
       open={isOpen}
-      text={translate('DialogTrust.dialogTrustDescription', { username })}
       title={translate('DialogTrust.dialogTrustTitle', { username })}
       onClose={handleTrustClose}
       onConfirm={handleTrust}
-    />
+    >
+      <Box display="flex" justifyContent="center" mb={2}>
+        <Avatar address={address} size="medium" />
+      </Box>
+      <Typography className={classes.paragraph} paragraph>
+        {translate('DialogTrust.dialogTrustDescription', { username })}
+      </Typography>
+      <ExternalLink className={classes.link} href={FAQ_URL} underline="always">
+        <Typography paragraph>
+          {translate('DialogAddMember.linkLearnMore')}
+        </Typography>
+      </ExternalLink>
+    </DialogPurple>
   );
 };
 
