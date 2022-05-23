f9fc359a8aba263a147560f01cb7d8d8a611d72d
mikozet <mikozet2@gmail.com>
2022-05-23 12:51:21 +0200

    Remove circle from how we display  circle balance

--- src/components/BalanceDisplay.js
+++ src/components/BalanceDisplay.js
@@ -1,37 +1,32 @@
+import { Box, Typography } from '@material-ui/core';
 import { CircularProgress } from '@material-ui/core';
-import { Box, Grid, Paper, Tooltip, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Logo from '~/components/Logo';
 import { useUpdateLoop } from '~/hooks/update';
-import translate from '~/services/locale';
 import { checkCurrentBalance, checkTokenState } from '~/store/token/actions';
 import { IconCircles } from '~/styles/icons';
-import { ISSUANCE_RATE_MONTH } from '~/utils/constants';
 import { formatCirclesValue } from '~/utils/format';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    maxWidth: theme.spacing(70),
-    margin: '0 auto',
-  },
-  grid: {
-    '&>*': {
-      flexGrow: 0,
-      flexBasis: 'auto',
-    },
+  box: {
+    textAlign: 'center',
+    position: 'relative',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   balance: {
-    fontSize: '2.5rem',
+    fontSize: '48px',
     fontWeight: theme.typography.fontWeightRegular,
+    padding: '20px',
   },
   balanceIcon: {
     position: 'relative',
     top: 2,
     marginRight: theme.spacing(0.5),
-    fontSize: '2rem',
+    fontSize: '2.3rem',
   },
 }));
 
@@ -45,45 +40,25 @@ const BalanceDisplay = () => {
     await dispatch(checkCurrentBalance());
   });
 
-  // Token is apparently deployed, wait for the incoming data
-  const isLoading =
-    (token.balance === null || token.balance === '0') && !safe.pendingNonce;
+  const isLoading = token.balance === null && !safe.pendingNonce;
+  let tokenBalance;
+  if (token.balance !== null) {
+    tokenBalance = formatCirclesValue(token.balance);
+  } else {
+    tokenBalance = 0;
+  }
 
   return (
-    <Tooltip
-      arrow
-      title={translate('BalanceDisplay.tooltipYourBalance', {
-        rate: ISSUANCE_RATE_MONTH,
-      })}
-    >
-      <Paper className={classes.paper} variant="outlined">
-        <Box p={2.5}>
-          <Grid
-            alignItems="center"
-            className={classes.grid}
-            container
-            justify="center"
-            spacing={2}
-          >
-            <Grid item xs>
-              <Logo size="small" />
-            </Grid>
-            <Grid item xs>
-              {isLoading ? (
-                <CircularProgress />
-              ) : (
-                <Typography className={classes.balance} component="span">
-                  <IconCircles className={classes.balanceIcon} />
-                  {token.balance !== null
-                    ? formatCirclesValue(token.balance)
-                    : 0}
-                </Typography>
-              )}
-            </Grid>
-          </Grid>
-        </Box>
-      </Paper>
-    </Tooltip>
+    <Box className={classes.box}>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <Typography className={classes.balance}>
+          <IconCircles className={classes.balanceIcon} />
+          {tokenBalance}
+        </Typography>
+      )}
+    </Box>
   );
 };
 
