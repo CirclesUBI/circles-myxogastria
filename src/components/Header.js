69bc037398e53e009d9d90f02cdd6cd67a751cb3
mikozet <mikozet2@gmail.com>
2022-05-25 14:11:03 +0200

    Add behaviour once user scrolls to Header  component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,30 +7,66 @@ import React from 'react';
 import { useCustomScrollTrigger } from '~/hooks/scrollTrigger';
 
 const useStyles = makeStyles((theme) => ({
+  headerContainer: {
+    position: 'fixed',
+    width: '100%',
+    // height: '64px',
+    left: 0,
+    zIndex: '1000',
+  },
   appBar: {
-    backgroundColor: theme.palette.background.default,
+    opacity: 0,
+    '&.MuiPaper-root': {
+      transition: 'all 0.15s',
+      background: (props) => {
+        if (props.isOrganization) {
+          return theme.custom.gradients.violet;
+        } else {
+          return theme.custom.gradients.greenBlueHeader;
+        }
+      },
+      boxShadow: '0px 0px 0px rgba(0, 0, 0, 0.25)',
+    },
+    height: '64px',
+  },
+  isScrolled: {
+    '&.MuiAppBar-root': {
+      background: (props) => {
+        if (props.isOrganization) {
+          return theme.custom.gradients.violet;
+        } else {
+          return theme.custom.gradients.greenBlueHeader;
+        }
+      },
+      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      opacity: 1,
+      transition: 'opacity 0.15s',
+    },
   },
   toolbar: {
     minHeight: theme.custom.components.appBarHeight,
     justifyContent: 'space-between',
+    position: 'relative',
+    zIndex: '10000',
   },
 }));
 
 const Header = ({ children, className, ...props }) => {
-  const classes = useStyles();
+  const classes = useStyles(props);
 
   const isScrolled = useCustomScrollTrigger();
 
   return (
-    <AppBar
-      className={clsx(classes.appBar, className)}
-      color="transparent"
-      elevation={isScrolled ? 2 : 0}
-      position="fixed"
-      {...props}
-    >
+    <div className={classes.headerContainer}>
+      <AppBar
+        className={clsx(classes.appBar, className, {
+          [classes.isScrolled]: isScrolled,
+        })}
+        color="transparent"
+        {...props}
+      ></AppBar>
       <Toolbar className={classes.toolbar}>{children}</Toolbar>
-    </AppBar>
+    </div>
   );
 };
 
