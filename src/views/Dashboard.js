ca3bfe09d5ac2b3c7c3cbca3b15dee38b79f0269
mikozet <mikozet2@gmail.com>
2022-05-31 14:15:29 +0200

    Add info component to dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -16,6 +16,7 @@ import { Link, generatePath, useHistory } from 'react-router-dom';
 import { MY_PROFILE_PATH, SEARCH_PATH, SEND_PATH } from '~/routes';
 
 import AppNote from '~/components/AppNote';
+import AppNototificationInfo from '~/components/AppNotificationInfo';
 import BalanceDisplay from '~/components/BalanceDisplay';
 import ButtonSend from '~/components/ButtonSend';
 import CenteredHeading from '~/components/CenteredHeading';
@@ -153,6 +154,7 @@ const Dashboard = () => {
           <Box my={2}>
             <DashboardSearch />
           </Box>
+          <AppNototificationInfo />
           <LastInteractions />
         </Container>
       </View>
