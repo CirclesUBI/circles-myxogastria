d2fb08c70434352ce467170d9e1093dbb23bc2e7
Louise Linné <linne.louise@gmail.com>
2022-06-10 14:22:28 +0200

    Fix missing clsx import in Dashboard after merge

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -6,6 +6,7 @@ import {
   IconButton,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import React, { Fragment, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
