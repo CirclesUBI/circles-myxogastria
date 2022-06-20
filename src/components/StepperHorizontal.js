50f06667c45adc2e7e8505324c7691d71534ed9d
mikozet <mikozet2@gmail.com>
2022-06-20 12:28:33 +0200

    Moove some colors logic to variable to reuse

--- src/components/StepperHorizontal.js
+++ src/components/StepperHorizontal.js
@@ -6,85 +6,82 @@ import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const useStyles = makeStyles((theme) => ({
-  stepper: {
-    '& .MuiStepConnector-horizontal': {
-      left: 'calc(-50% + 12px)',
-      right: 'calc(50% + 12px)',
-      top: '11px',
-      [theme.breakpoints.up('md')]: {
-        top: '15px',
-        left: 'calc(-50% + 16px)',
-        right: 'calc(50% + 16px)',
-      },
-
-      '& .MuiStepConnector-lineHorizontal': {
-        borderTopWidth: '2.5px',
+const useStyles = makeStyles((theme) => {
+  const colorTheme = (isOrganization) =>
+    isOrganization
+      ? theme.custom.colors.violet
+      : theme.custom.colors.fountainBlue;
+  return {
+    stepper: {
+      '& .MuiStepConnector-horizontal': {
+        left: 'calc(-50% + 12px)',
+        right: 'calc(50% + 12px)',
+        top: '11px',
         [theme.breakpoints.up('md')]: {
-          borderTopWidth: '3px',
+          top: '15px',
+          left: 'calc(-50% + 16px)',
+          right: 'calc(50% + 16px)',
+        },
+
+        '& .MuiStepConnector-lineHorizontal': {
+          borderTopWidth: '2.5px',
+          [theme.breakpoints.up('md')]: {
+            borderTopWidth: '3px',
+          },
+        },
+
+        '&.MuiStepConnector-active, &.MuiStepConnector-completed': {
+          '& .MuiStepConnector-lineHorizontal': {
+            background: colorTheme,
+            border: '1.5px solid transparent',
+          },
         },
       },
+    },
+    stepperButton: {
+      '& .MuiSvgIcon-root.MuiStepIcon-root': {
+        color: '#fff',
+        fontWeight: '600',
+        fontSize: '1.3rem',
+        borderRadius: '100%',
+        border: `2px solid ${theme.custom.colors.grayDark}`,
+        [theme.breakpoints.up('md')]: {
+          fontSize: '1.8rem',
+        },
 
-      '&.MuiStepConnector-active, &.MuiStepConnector-completed': {
-        '& .MuiStepConnector-lineHorizontal': {
+        '& .MuiStepIcon-text': {
+          fill: theme.custom.colors.grayDark,
+        },
+
+        '&.MuiStepIcon-active, &.MuiStepIcon-completed': {
           background: (isOrganization) =>
             isOrganization
               ? theme.custom.colors.violet
               : theme.custom.colors.fountainBlue,
-          border: '1.5px solid transparent',
+          border: '2px solid transparent',
+
+          '& .MuiStepIcon-text': {
+            fill: colorTheme,
+          },
+        },
+      },
+
+      '& .MuiStepLabel-label.MuiStepLabel-alternativeLabel': {
+        fontSize: '10px',
+        fontWeight: '500',
+        color: theme.custom.colors.grayDark,
+        marginTop: '8px',
+        [theme.breakpoints.up('md')]: {
+          fontSize: '12px',
+        },
+
+        '&.MuiStepLabel-completed, &.MuiStepLabel-active': {
+          color: colorTheme,
         },
       },
     },
-  },
-  stepperButton: {
-    '& .MuiSvgIcon-root.MuiStepIcon-root': {
-      color: '#fff',
-      fontWeight: '600',
-      fontSize: '1.3rem',
-      borderRadius: '100%',
-      border: `2px solid ${theme.custom.colors.grayDark}`,
-      [theme.breakpoints.up('md')]: {
-        fontSize: '1.8rem',
-      },
-
-      '& .MuiStepIcon-text': {
-        fill: theme.custom.colors.grayDark,
-      },
-
-      '&.MuiStepIcon-active, &.MuiStepIcon-completed': {
-        background: (isOrganization) =>
-          isOrganization
-            ? theme.custom.colors.violet
-            : theme.custom.colors.fountainBlue,
-        border: '2px solid transparent',
-
-        '& .MuiStepIcon-text': {
-          fill: (isOrganization) =>
-            isOrganization
-              ? theme.custom.colors.violet
-              : theme.custom.colors.fountainBlue,
-        },
-      },
-    },
-
-    '& .MuiStepLabel-label.MuiStepLabel-alternativeLabel': {
-      fontSize: '10px',
-      fontWeight: '500',
-      color: theme.custom.colors.grayDark,
-      marginTop: '8px',
-      [theme.breakpoints.up('md')]: {
-        fontSize: '12px',
-      },
-
-      '&.MuiStepLabel-completed, &.MuiStepLabel-active': {
-        color: (isOrganization) =>
-          isOrganization
-            ? theme.custom.colors.violet
-            : theme.custom.colors.fountainBlue,
-      },
-    },
-  },
-}));
+  };
+});
 
 const StepperHorizontal = ({ steps, activeStep, isOrganization }) => {
   const classes = useStyles(isOrganization);
