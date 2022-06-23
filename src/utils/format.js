9e7a14d30bb88e12f6ec17a3efaeed2032f5b65b
Louise Linné <linne.louise@gmail.com>
2022-06-23 13:08:14 +0200

    Fix and improve tests of formarCirclesValue and add method description

--- src/utils/format.js
+++ src/utils/format.js
@@ -2,6 +2,13 @@ import { crcToTc } from '@circles/timecircles';
 
 import web3 from '~/services/web3';
 
+/**
+ * Will convert and format Freckles to Time Circles
+ * @param {string} valueInFreckles
+ * @param {Date | number} timestamp date of Time Circles conversion (default present time)
+ * @param {number} decimals number of decimals in returned Time Circles value (default 2)
+ * @returns formatted Time Circles value
+ */
 export function formatCirclesValue(
   valueInFreckles,
   timestamp = Date.now(),
