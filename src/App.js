c8f2f3b542b040a63e0364d63fc4363417d38fd9
mikozet <mikozet2@gmail.com>
2022-06-26 08:10:48 +0200

    Update App component with PinkShadow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { Box, IconButton } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import { SnackbarProvider } from 'notistack';
-import React, { useEffect, useRef, useState } from 'react';
-import { use100vh } from 'react-div-100vh';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from '~/routes';
 
 import Notifications from '~/components/Notifications';
+import PinkShadow from '~/components/PinkShadow';
 import SpinnerOverlay from '~/components/SpinnerOverlay';
 import UBI from '~/components/UBI';
 import { initializeApp } from '~/store/app/actions';
@@ -17,14 +16,6 @@ import { IconAlert, IconClose } from '~/styles/icons';
 import logError from '~/utils/debug';
 
 const useStyles = makeStyles((theme) => ({
-  app: {
-    minWidth: theme.custom.components.appMinWidth,
-    maxWidth: theme.custom.components.appMaxWidth,
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '0 auto',
-  },
   snackbar: {},
   // @NOTE: Hacky use of !important, see related issue:
   // https://github.com/iamhosseindhv/notistack/issues/305
@@ -47,32 +38,13 @@ const useStyles = makeStyles((theme) => ({
   snackbarIconVariant: {
     marginRight: theme.spacing(1),
   },
-  pinkShadow: {
-    width: '100%',
-    height: '303px',
-    position: 'fixed',
-    bottom: '0',
-    background: theme.custom.gradients.pinkShade,
-    opacity: 0,
-    transition: 'opacity 0.1s',
-  },
-  isPinkShadow: {
-    opacity: '1',
-    transition: 'opacity 0.1s',
-  },
 }));
 
 const App = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const app = useSelector((state) => state.app);
-  const [isPinkShadow, setIsPinkShadow] = useState(false);
-
-  const ref = useRef();
   const notistackRef = useRef();
-
-  // Fix issue where there was always one pixel too much in the calculation
-  const height = use100vh() - 1;
 
   const onClickDismiss = (notificationId) => () => {
     notistackRef.current.closeSnackbar(notificationId);
@@ -92,32 +64,6 @@ const App = () => {
   }, [dispatch]);
 
   const SnackbarIcon = <IconAlert className={classes.snackbarIconVariant} />;
-
-  useEffect(() => {
-    const onScroll = () => {
-      const { scrollHeight, clientHeight } = ref.current;
-      if (scrollHeight <= clientHeight + window.pageYOffset + 1) {
-        setIsPinkShadow(false);
-      } else {
-        setIsPinkShadow(true);
-      }
-    };
-
-    window.addEventListener('scroll', onScroll);
-
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
-
-  useEffect(() => {
-    const { scrollHeight, clientHeight } = ref.current;
-
-    if (
-      scrollHeight > clientHeight &&
-      !(scrollHeight <= clientHeight + window.pageYOffset + 1)
-    ) {
-      setIsPinkShadow(true);
-    }
-  }, [ref.current?.scrollHeight]);
 
   return (
     <SnackbarProvider
@@ -143,17 +89,12 @@ const App = () => {
       ref={notistackRef}
     >
       <Router>
-        <Box
-          className={clsx(classes.pinkShadow, {
-            [classes.isPinkShadow]: isPinkShadow,
-          })}
-        ></Box>
-        <Box className={classes.app} ref={ref} style={{ height }}>
+        <PinkShadow>
           <UBI />
           <Notifications />
           <SpinnerOverlay isVisible={app.isLoading} />
           <Routes />
-        </Box>
+        </PinkShadow>
       </Router>
     </SnackbarProvider>
   );
