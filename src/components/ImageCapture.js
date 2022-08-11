c175375f6b7319f30f242f084d9c2d436fab4d3d
mikozet <mikozet2@gmail.com>
2022-08-11 15:33:02 +0200

    Update ImageCapture

--- src/components/ImageCapture.js
+++ src/components/ImageCapture.js
@@ -1,8 +1,9 @@
-import { Box } from '@material-ui/core';
+import { Box, CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { createRef, useCallback } from 'react';
 
 import Button from '~/components/Button';
@@ -12,17 +13,35 @@ const useStyles = makeStyles((theme) => ({
   videoContainer: {
     marginBottom: '25px',
     height: '198px',
+    clipPath: 'circle(99px at center)',
     [theme.breakpoints.up('sm')]: {
       minHeight: '263px',
+      clipPath: 'circle(130px at center)',
     },
+    overflow: 'hidden',
   },
   imageCanvas: {
     display: 'none',
+  },
+  loadingMask: {
+    background: theme.custom.colors.cornflowerBlue,
+    opacity: 0.5,
+    position: 'relative',
+  },
+  loadingIndicator: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  imageCaptureContainer: {
+    position: 'relative',
   },
 }));
 
 const ImageCapture = ({ onCapture, onError, width, userMediaConfig }) => {
   const classes = useStyles();
+  const [isLoading, setIsLoading] = useState(true);
 
   const playerRef = createRef();
   const canvasRef = createRef();
@@ -33,6 +52,7 @@ const ImageCapture = ({ onCapture, onError, width, userMediaConfig }) => {
       .then((stream) => {
         if (playerRef.current) {
           playerRef.current.srcObject = stream;
+          setIsLoading(false);
         }
         tracks.current = stream.getTracks();
       })
@@ -66,24 +86,31 @@ const ImageCapture = ({ onCapture, onError, width, userMediaConfig }) => {
         onCapture({
           blob,
           png: pngData,
-          file: new File([pngData], `${new Date().getTime()}.png`),
+          file: new File([blob], `${new Date().getTime()}.png`, {
+            type: 'image/png',
+          }),
         });
       });
     }
   }, [onCapture, canvasRef, playerRef]);
 
   return (
-    <>
+    <Box className={classes.imageCaptureContainer}>
       <Box className={classes.videoContainer}>
+        {isLoading && (
+          <Box className={clsx(classes.videoContainer, classes.loadingMask)}>
+            <Box className={classes.loadingIndicator}>
+              <CircularProgress />
+            </Box>
+          </Box>
+        )}
         <video autoPlay playsInline ref={playerRef} width={width}></video>
       </Box>
-      <Box>
-        <Button fullWidth isPrimary onClick={captureImage}>
-          {translate('EditProfile.optionCapture')}
-        </Button>
-        <canvas className={classes.imageCanvas} ref={canvasRef} />
-      </Box>
-    </>
+      <canvas className={classes.imageCanvas} ref={canvasRef} />
+      <Button disabled={isLoading} fullWidth isPrimary onClick={captureImage}>
+        {translate('EditProfile.optionCapture')}
+      </Button>
+    </Box>
   );
 };
 
