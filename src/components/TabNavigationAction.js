d62de841117045f354b84f6c14260051459843e1
mikozet <mikozet2@gmail.com>
2022-10-24 09:53:19 +0200

    Add item counter for tabs

--- src/components/TabNavigationAction.js
+++ src/components/TabNavigationAction.js
@@ -1,4 +1,4 @@
-import { Tab } from '@material-ui/core';
+import { Box, Tab } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
@@ -10,24 +10,59 @@ const useStyles = makeStyles((theme) => ({
     textTransform: 'none',
     fontWeight: theme.typography.fontWeightLight,
     fontSize: 13,
+    position: 'relative',
+  },
+  tabContainer: {
+    position: 'relative',
+  },
+  itemsCounterContainer: {
+    position: 'absolute',
+    background: theme.custom.colors.fountainBlue,
+    color: theme.custom.colors.whiteAlmost,
+    top: '3px',
+    left: '48px',
+    borderRadius: '50%',
+    padding: '5px',
+    height: '17px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    [theme.breakpoints.up('sm')]: {
+      left: '93px',
+    },
+  },
+  number: {
+    display: 'inline-block',
+    fontSize: '12px',
+    fontWeight: '500',
+    marginBottom: '-3px',
   },
 }));
 
-const TabNavigationAction = ({ className, ...props }) => {
+const TabNavigationAction = ({ className, itemsCounter, ...props }) => {
   const classes = useStyles();
 
   return (
-    <Tab
-      classes={{
-        root: clsx(className, classes.tab),
-      }}
-      {...props}
-    />
+    <Box className={classes.tabContainer}>
+      <Tab
+        classes={{
+          root: clsx(className, classes.tab),
+        }}
+        {...props}
+      ></Tab>
+      {itemsCounter && (
+        <Box className={classes.itemsCounterContainer}>
+          <span className={classes.number}>{itemsCounter}</span>
+        </Box>
+      )}
+    </Box>
   );
 };
 
 TabNavigationAction.propTypes = {
   className: PropTypes.string,
+  itemsCounter: PropTypes.number,
 };
 
 export default TabNavigationAction;
