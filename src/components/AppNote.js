18267ce3aea21d0ff892f023967cdf134366babd
mikozet <mikozet2@gmail.com>
2022-10-28 16:06:57 +0200

    Add icon and color choice for Banner messages

--- src/components/AppNote.js
+++ src/components/AppNote.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import theme from '../styles/theme';
+
 import HumbleAlert from '~/components/HumbleAlert';
 
 const useStyles = makeStyles(() => ({
@@ -12,24 +14,53 @@ const useStyles = makeStyles(() => ({
 }));
 
 const notification = {
-  validation: process.env.USER_NOTIFICATION_VALIDATION,
-  dashboard: process.env.USER_NOTIFICATION_DASHBOARD,
-  default: process.env.USER_NOTIFICATION,
-  error: process.env.USER_NOTIFICATION_ERROR,
-  login: process.env.USER_NOTIFICATION_LOGIN,
-  onboarding: process.env.USER_NOTIFICATION_ONBOARDING,
+  validation: {
+    color: process.env.USER_NOTIFICATION_VALIDATION_COLOR,
+    icon: process.env.USER_NOTIFICATION_VALIDATION_ICON,
+    text: process.env.USER_NOTIFICATION_VALIDATION,
+  },
+  dashboard: {
+    color: process.env.USER_NOTIFICATION_DASHBOARD_COLOR,
+    icon: process.env.USER_NOTIFICATION_DASHBOARD_ICON,
+    text: process.env.USER_NOTIFICATION_DASHBOARD,
+  },
+  default: {
+    color: theme.custom.colors.fountainBlue,
+    icon: 'IconAlert',
+    text: process.env.USER_NOTIFICATION,
+  },
+  error: {
+    color: process.env.USER_NOTIFICATION_ERROR_COLOR,
+    icon: process.env.USER_NOTIFICATION_ERROR_ICON,
+    text: process.env.USER_NOTIFICATION_ERROR,
+  },
+  login: {
+    color: process.env.USER_NOTIFICATION_LOGIN_COLOR,
+    icon: process.env.USER_NOTIFICATION_LOGIN_ICON,
+    text: process.env.USER_NOTIFICATION_LOGIN,
+  },
+  onboarding: {
+    color: process.env.USER_NOTIFICATION_ONBOARDING_COLOR,
+    icon: process.env.USER_NOTIFICATION_ONBOARDING_ICON,
+    text: process.env.USER_NOTIFICATION_ONBOARDING,
+  },
 };
 
 const AppNote = ({ messageVersion }) => {
   const classes = useStyles();
   const choice = messageVersion ? messageVersion : 'default';
-  const message = notification[choice]
-    ? notification[choice]
-    : notification['default'];
+  const message = notification[choice].text
+    ? notification[choice].text
+    : notification['default'].text;
 
   return message ? (
     <Box className={classes.appNoteContainer} my={2}>
-      <HumbleAlert>{message}</HumbleAlert>
+      <HumbleAlert
+        color={notification[choice].color}
+        icon={notification[choice].icon}
+      >
+        {message}
+      </HumbleAlert>
     </Box>
   ) : null;
 };
