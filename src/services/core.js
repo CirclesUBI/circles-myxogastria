328492e4e6434c64cd8ce335db74dfa46e8ac884
llunaCreixent <elesmp@protonmail.com>
2022-11-23 10:00:48 +0100

    Add new option variable in core constructor

--- src/services/core.js
+++ src/services/core.js
@@ -11,6 +11,7 @@ const core = new CirclesCore(web3, {
   relayServiceEndpoint: process.env.RELAY_SERVICE_EXTERNAL,
   safeMasterAddress: process.env.SAFE_ADDRESS,
   subgraphName: process.env.SUBGRAPH_NAME,
+  fallbackHandlerAddress: process.env.SAFE_DEFAULT_CALLBACK_HANDLER,
 });
 
 async function requestCore(moduleName, method, options) {
