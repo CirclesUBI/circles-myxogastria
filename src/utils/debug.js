205f38cd97588c25d1072aae188a825712e1239b
miko <mikozet2@gmail.com>
2022-12-02 14:58:55 +0400

    Update error handling

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
 
 import core from '~/services/core';
+import translate from '~/services/locale';
 import { captureException } from '~/services/sentry';
 
-const { TransferError, RequestError, CoreError } = core.errors;
+const { ErrorCodes, TransferError, RequestError, CoreError } = core.errors;
 
 export function formatErrorMessage(error) {
   // Display internal error message to user for debugging purposes
@@ -42,4 +43,19 @@ export default function logError(error) {
   }
 
   console.groupEnd();
+}
+
+export function translateErrorForUser(error) {
+  let text;
+  if (error instanceof CoreError) {
+    if (error.code == ErrorCodes.INSUFFICIENT_FUNDS) {
+      text = translate('ErrorCodes.CoreErrorInsufficientFunds');
+    }
+  } else if (error instanceof TransferError) {
+    /*eslint-disable no-empty */
+  } else if (error instanceof RequestError) {
+    /*eslint-enable no-empty */
+  }
+
+  return text;
 }
