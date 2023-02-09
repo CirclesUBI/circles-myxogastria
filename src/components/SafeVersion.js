21212f07201bc4c0e3f0eed088af7fb268b9fdaa
mikozet <95082155+mikozet@users.noreply.github.com>
2023-02-09 16:27:20 +0100

    Remove Safe version update notification (#596)
    * Remove Safe version update notification
    
    * Remove redundant import

--- src/components/SafeVersion.js
+++ src/components/SafeVersion.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import core from '~/services/core';
-import translate from '~/services/locale';
 import { getSafeVersion, setSafeVersion } from '~/services/safe';
 import notify, { NotificationsTypes } from '~/store/notifications/actions';
 import { updateSafeVersion } from '~/store/safe/actions';
@@ -45,19 +44,6 @@ const SafeVersion = () => {
 
         // .. and update the Safe!
         await dispatch(updateSafeVersion());
-
-        const version = await core.safe.getVersion(safe.currentAccount);
-
-        // Display the action to the user
-        dispatch(
-          notify({
-            text: translate('SafeVersion.infoUpdatedVersion', {
-              version: version,
-            }),
-            type: NotificationsTypes.INFO,
-            timeout: 10000,
-          }),
-        );
       } catch (error) {
         dispatch(
           notify({
