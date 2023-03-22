088b92488b7ec0d654897d44619d56b1079a46e6
Louise Linné <linne.louise@gmail.com>
2023-03-22 17:08:37 +0100

    Add transaction data to csv file (except other account's username)

--- src/utils/fileExports.js
+++ src/utils/fileExports.js
@@ -1,12 +1,54 @@
 import fileDownload from 'js-file-download';
 import { DateTime } from 'luxon';
 
+import core from '~/services/core';
+import resolveTxHash from '~/services/transfer';
+import { formatCirclesValue } from '~/utils/format';
+
+const { ActivityFilterTypes } = core.activity;
+
 const getBalance = (/*dateTime, safe*/) => {
   return 25; // Dummy balance
 };
 
 const formatDate = (/*dateTime*/) => {
   return '01.01.2022'; // Dummy date
+};
+
+const getActivities = async (safeAddress) => {
+  try {
+    const { activities /*, lastTimestamp*/ } = await core.activity.getLatest(
+      safeAddress,
+      ActivityFilterTypes.TRANSFERS,
+      20,
+    );
+    // console.log('CAT --- ', activities, lastTimestamp);
+    return activities;
+  } catch (e) {
+    // TODO: HANDLE
+    // console.log(e);
+  }
+};
+
+const formatActivities = async (activities, safeAddress) => {
+  return await Promise.all(
+    activities.map(async (activity) => {
+      const { to, from, value } = activity.data;
+      const timestamp = activity.timestamp;
+      const message = await resolveTxHash(activity.transactionHash);
+      const otherSafeAddress = to === safeAddress ? from : to;
+      const valueSign = from === safeAddress ? '-' : '+';
+      const date = DateTime.fromSeconds(timestamp);
+      const valueInTimeCircles = formatCirclesValue(value, date, 2, false);
+      return [
+        date.toFormat('dd.LL.yyyy'),
+        'Placeholder Name',
+        otherSafeAddress,
+        message || '-',
+        valueSign.concat(valueInTimeCircles),
+      ].join(';');
+    }),
+  );
 };
 
 const generateCsvContent = (
@@ -36,7 +78,7 @@ const generateCsvContent = (
 /**
  * Exports account statement as csv file
  */
-export function downloadCsvStatement(
+export async function downloadCsvStatement(
   accountName,
   safeAddress,
   startDate = DateTime.now(),
@@ -51,10 +93,18 @@ export function downloadCsvStatement(
    * format transactions
    * filename includes dates
    */
+
+  // verify date order - later
+  // DateTime.fromISO(lastSeenAt) > DateTime.fromISO(createdAt);
+
+  // get activity
+  const activities = await getActivities(safeAddress);
+  const csvTransactions = await formatActivities(activities, safeAddress);
+  // console.log(csvTransactions);
+
   const endBalance = getBalance(endDate, safeAddress);
   const startBalance = getBalance(startDate, safeAddress);
   const demurrage = 2;
-  const csvTransactions = ['trans1', 'trans2', 'trans3'];
   const csvString = generateCsvContent(
     accountName,
     safeAddress,
