633913b1d4b67f03074457f5b5e940e6ca1681db
Louise Linné <linne.louise@gmail.com>
2023-03-22 14:28:43 +0100

    Set csv basic format

--- src/utils/fileExports.js
+++ src/utils/fileExports.js
@@ -1,10 +1,69 @@
 import fileDownload from 'js-file-download';
 
+const getBalance = (/*dateTime, safe*/) => {
+  return 25; // Dummy balance
+};
+
+const formatDate = (/*dateTime*/) => {
+  return '01.01.2022'; // Dummy date
+};
+
+const generateCsvContent = (
+  accountName,
+  safeAddress,
+  startDate,
+  endDate,
+  startBalance,
+  endBalance,
+  demurrage,
+  csvTransactions,
+) => {
+  return [
+    'Circles Account Statement',
+    `account_name; ${accountName}`,
+    `account_safe_address; ${safeAddress}`,
+    `period_start_date"; ${formatDate(startDate)}`,
+    `period_end_date; ${formatDate(endDate)}`,
+    `balance_on_end_date; ${endBalance}`,
+    `demurrage_during_selected_period, ${demurrage}`,
+    `date; to_or_from_username; to_or_from_safe_address; tranfer_note, amount_in_circles`,
+    ...csvTransactions,
+    `balance_on_start_date; ${startBalance}`,
+  ].join('\n');
+};
+
 /**
- * Exports a csv file
+ * Exports account statement as csv file
  */
-export function downloadCsvStatement() {
-  const csvString = 'letter; number; \n a; 2;';
+export function downloadCsvStatement(
+  accountName,
+  safeAddress,
+  startDate,
+  endDate,
+) {
+  /* TODO:
+   * verify date order
+   * get activity
+   * filter activity
+   * call calc balance
+   * call calc demurrage
+   * format transactions
+   * filename includes dates
+   */
+  const endBalance = getBalance(endDate, safeAddress);
+  const startBalance = getBalance(startDate, safeAddress);
+  const demurrage = 2;
+  const csvTransactions = ['trans1', 'trans2', 'trans3'];
+  const csvString = generateCsvContent(
+    accountName,
+    safeAddress,
+    startDate,
+    endDate,
+    startBalance,
+    endBalance,
+    demurrage,
+    csvTransactions,
+  );
   const filename = 'export.csv';
   fileDownload(csvString, filename);
   return;
