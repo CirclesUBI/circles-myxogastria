5e803ccf99d3d5cd57febe77108a7234326ff519
Louise Linné <linne.louise@gmail.com>
2023-03-23 01:09:57 +0100

    Add function documentation and reverse order of transactions

--- src/utils/fileExports.js
+++ src/utils/fileExports.js
@@ -14,6 +14,19 @@ const { ActivityFilterTypes } = core.activity;
 
 const formatDate = (dateTime) => dateTime.toFormat(`dd.LL.yyyy`).toString();
 
+/**
+ * Generates a csv string including line breaks based on input strings
+ * @param {string} accountName The name of the exporting account
+ * @param {string} safeAddress The corresponding safe address
+ * @param {string} startDate Starting date of period to export
+ * @param {string} endDate Ending date of period to export
+ * @param {string} startBalance Start balance
+ * @param {string} endBalance End balance
+ * @param {string} demurrage Demurrage
+ * @param {string} sumOfTransactions Sum of transactions in the specified period
+ * @param {string[]} csvTransactions List of csv strings per transaction in the specified period
+ * @returns A string containing the complete csv content of an account statement
+ */
 const generateCsvContent = (
   accountName,
   safeAddress,
@@ -30,8 +43,8 @@ const generateCsvContent = (
     '',
     `Account_name; ${accountName}`,
     `Account_safe_address; ${safeAddress}`,
-    `Period_start_date; ${formatDate(startDate)}`,
-    `Period_end_date; ${formatDate(endDate)}`,
+    `Period_start_date; ${startDate}`,
+    `Period_end_date; ${endDate}`,
     '',
     `Balance_on_end_date; ${endBalance}`,
     `Demurrage_during_selected_period; ${demurrage}`,
@@ -45,14 +58,15 @@ const generateCsvContent = (
 };
 
 /**
- * @param {*} transactions
- * @param {*} safeAddress
- * @returns A csv string with line separated transactions in format
+ * Generates an array of csv formatted strings for given transactions
+ * @param {Object[]} transactions an array of transactions in the same format as from getTransactions()
+ * @param {string} safeAddress the safe address of the common sender/receiver of the transactions
+ * @returns An array of csv strings with line separated transactions in format
  * 'dd.mm.yyyy, Username, 0x00000000000000000000, Transaction message, -123.45'
  */
-const formatActivities = async (transactions, safeAddress) => {
+const formatTransactions = async (transactions, safeAddress) => {
   const transactionData = await Promise.all(
-    transactions.map(async (transaction) => {
+    transactions.reverse().map(async (transaction) => {
       const { to, from, valueInFreckles, txHash, date } = transaction;
       const message = await resolveTxHash(txHash);
       const otherSafeAddress = to === safeAddress ? from : to;
@@ -61,8 +75,9 @@ const formatActivities = async (transactions, safeAddress) => {
         valueInFreckles,
         date,
         NUMBER_OF_DECIMALS,
-        false, // important to not round normal, not down to get even numbers
+        false, // important to round normal, not down to get even numbers
       );
+
       return [
         formatDate(date),
         'placeholder name',
@@ -86,10 +101,10 @@ const formatActivities = async (transactions, safeAddress) => {
 };
 
 /**
- *
- * @param {*} transactions
- * @param {*} shouldBeInTc
- * @returns
+ * Calculates the sum of given transactions in CRC (default) or TC
+ * @param {Object[]} transactions an array of transactions in the same format as from getTransactions()
+ * @param {boolean} shouldBeInTc boolean flag specifying if sum should be in TimeCircles (default is CRC)
+ * @returns a sum of all transaction values in float format
  */
 const sumOfTransactions = (transactions, shouldBeInTc = false) => {
   if (shouldBeInTc) {
@@ -111,9 +126,10 @@ const sumOfTransactions = (transactions, shouldBeInTc = false) => {
 };
 
 /**
- *
- * @param {*} safeAddress
- * @returns
+ * Fetching and compiling a list transaction data objects
+ * @param {string} safeAddress
+ * @returns an array of Objects representing transaction with attributes:
+ * {to, from, valueInFreckles, valueInCircles, valueInTimeCircles, txHash, date, isNegative}
  */
 const getTransactions = async (safeAddress) => {
   try {
@@ -147,12 +163,14 @@ const getTransactions = async (safeAddress) => {
 };
 
 /**
- *
- * @param {*} accountName
- * @param {*} safeAddress
- * @param {*} startDate
- * @param {*} endDate
- * @returns
+ * Downloads a csv file with transaction specifications, sum of transactions, demurrage,
+ * start balance and end balance for specified account and period in time for a specific account.
+ * Downloaded file is similar to a bank statement.
+ * @param {string} accountName Name of the account in the export (only used for text)
+ * @param {string} safeAddress Safe address of account in question (to fetch data)
+ * @param {DateTime} startDate Start date of export period
+ * @param {DateTime} endDate End date of export period
+ * @returns nothing
  */
 export async function downloadCsvStatement(
   accountName,
@@ -194,12 +212,12 @@ export async function downloadCsvStatement(
   //  console.log({ balanceChangeTc, demurrage });
 
   // CSV formatting
-  const csvTransactions = await formatActivities(transactions, safeAddress);
+  const csvTransactions = await formatTransactions(transactions, safeAddress);
   const csvString = generateCsvContent(
     accountName,
     safeAddress,
-    startDate,
-    endDate,
+    formatDate(startDate),
+    formatDate(endDate),
     startBalanceTc.toFixed(NUMBER_OF_DECIMALS).toString(),
     endBalanceTc.toFixed(NUMBER_OF_DECIMALS).toString(),
     demurrage.toFixed(NUMBER_OF_DECIMALS).toString(),
