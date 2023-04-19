fff673964a90dbff3f20205d9d28ae84b73d03f9
louilinn <linne.louise@gmail.com>
2023-04-19 15:13:56 +0200

    633 export balance UI for first stage visual part and popup functionality with date selection (#649)
    * Basic UI for export statement
    
    * Add logic to end date input of export function, decrease size of dialog to match lines width
    
    ---------
    
    Co-authored-by: miko <mikozet2@gmail.com>

--- src/components/ActivityStreamWithTabs.js
+++ src/components/ActivityStreamWithTabs.js
@@ -1,3 +1,5 @@
+import { Box } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
 import PropTypes from 'prop-types';
 import qs from 'qs';
 import React, { Fragment, useCallback, useEffect, useState } from 'react';
@@ -8,20 +10,30 @@ import { ACTIVITIES_PATH } from '~/routes';
 
 import ActivityStream from '~/components/ActivityStream';
 import BadgeTab from '~/components/BadgeTab';
-import Button from '~/components/Button';
+//import Button from '~/components/Button';
+import ButtonIcon from '~/components/ButtonIcon';
+import DialogExportStatement from '~/components/DialogExportStatement';
 import TabNavigation from '~/components/TabNavigation';
 import TabNavigationAction from '~/components/TabNavigationAction';
-import { useIsOrganization, useUserdata } from '~/hooks/username';
+//import { useIsOrganization, useUserdata } from '~/hooks/username';
 import core from '~/services/core';
 import translate from '~/services/locale';
 import { loadMoreActivities, updateLastSeen } from '~/store/activity/actions';
 import { CATEGORIES } from '~/store/activity/reducers';
 import { IconConnections, IconTransactions } from '~/styles/icons';
-import { downloadCsvStatement } from '~/utils/fileExports';
+//import { downloadCsvStatement } from '~/utils/fileExports';
 
 const { ActivityFilterTypes } = core.activity;
 
 const DEFAULT_CATEGORY = ActivityFilterTypes.CONNECTIONS;
+
+const useStyles = makeStyles(() => ({
+  exportContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    margin: '-15px 0 30px',
+  },
+}));
 
 const QUERY_FILTER_MAP = {
   transfers: ActivityFilterTypes.TRANSFERS,
@@ -35,14 +47,16 @@ const filterToQuery = (filterName) => {
 };
 
 const ActivityStreamWithTabs = ({ basePath = ACTIVITIES_PATH }) => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
+  const [dialogOpen, setDialogOpen] = useState(true);
 
   const [categorySetByUser, setCategorySetByUser] = useState(false);
   const { categories, lastSeenAt } = useSelector((state) => state.activity);
-  const safeAddress = useSelector((state) => state.safe.currentAccount);
-  const { isOrganization } = useIsOrganization(safeAddress);
-  const { username } = useUserdata(safeAddress);
+  // const safeAddress = useSelector((state) => state.safe.currentAccount);
+  // const { isOrganization } = useIsOrganization(safeAddress);
+  // const { username } = useUserdata(safeAddress);
 
   // Get only new Activities and segregate them by category
   const newActivities = CATEGORIES.reduceRight((newActivities, category) => {
@@ -78,6 +92,14 @@ const ActivityStreamWithTabs = ({ basePath = ACTIVITIES_PATH }) => {
 
   const handleLoadMore = () => {
     dispatch(loadMoreActivities(selectedCategory));
+  };
+
+  const exportStatementBtnHandler = () => {
+    setDialogOpen(true);
+  };
+
+  const dialogCloseHandler = () => {
+    setDialogOpen(false);
   };
 
   const handleFilterSelection = useCallback(
@@ -144,12 +166,23 @@ const ActivityStreamWithTabs = ({ basePath = ACTIVITIES_PATH }) => {
           value={ActivityFilterTypes.CONNECTIONS}
         />
       </TabNavigation>
-      {selectedCategory === ActivityFilterTypes.TRANSFERS && isOrganization && (
-        <Button onClick={() => downloadCsvStatement(username, safeAddress)}>
-          Export Statement Download
-        </Button>
-      )}
-      {/* TODO: styling */}
+      {
+        // {selectedCategory === ActivityFilterTypes.TRANSFERS && isOrganization && (
+        //   <Button onClick={() => downloadCsvStatement(username, safeAddress)}>
+        //     Export Statement Download
+        //   </Button>
+        // )}
+        // {/* TODO: styling */}
+      }
+      <Box className={classes.exportContainer}>
+        <ButtonIcon icon="IconUnion" onClick={exportStatementBtnHandler}>
+          {translate('ExportStatement.exportBtnText')}
+        </ButtonIcon>
+      </Box>
+      <DialogExportStatement
+        dialogOpen={dialogOpen}
+        onCloseHandler={dialogCloseHandler}
+      />
       <ActivityStream
         activities={activity.activities}
         isLoading={isLoading}
