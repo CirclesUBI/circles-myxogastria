4019d559da97da594d140bb4a1a25c5015c673f8
Louise Linné <linne.louise@gmail.com>
2023-06-19 11:29:37 +0200

    Update mui imports compatible with new mui version

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -6,8 +6,8 @@ import {
   Divider,
   IconButton,
   Zoom,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+} from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
--- src/components/TabNavigationAction.js
+++ src/components/TabNavigationAction.js
@@ -1,5 +1,5 @@
-import { Box, Tab } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Box, Tab } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
