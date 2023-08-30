0290a31bcf871888d138b18d435f3c5b2094111f
miko <mikozet2@gmail.com>
2023-08-30 13:24:33 +0400

    Update TabNavigationAction component

--- src/components/TabNavigationAction.js
+++ src/components/TabNavigationAction.js
@@ -1,15 +1,16 @@
-import { Tab } from '@mui/material';
+import { Box, Tab } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { fontSizeSmaller } from '~/styles/fonts';
+import { fontSizeSmaller, fontWeightMedium } from '~/styles/fonts';
 
 const useStyles = makeStyles((theme) => ({
   tab: {
     padding: '11px 8px 0',
     textTransform: 'none',
+    position: 'relative',
     fontWeight: theme.typography.fontWeightRegular,
     fontSize: fontSizeSmaller,
     color: theme.custom.colors.lily,
@@ -39,7 +40,6 @@ const useStyles = makeStyles((theme) => ({
       },
     },
     '&.MuiTab-labelIcon': {
-      minHeight: 'auto',
       paddingBottom: '3px',
 
       '& .MuiTab-wrapper > *:first-child': {
@@ -50,24 +50,60 @@ const useStyles = makeStyles((theme) => ({
       marginBottom: '3px',
     },
   },
+  tabContainer: {
+    position: 'relative',
+    width: '33.33%',
+    display: 'flex',
+  },
+  itemsCounterContainer: {
+    position: 'absolute',
+    background: theme.custom.colors.fountainBlue,
+    color: theme.custom.colors.whiteAlmost,
+    top: '3px',
+    left: '48px',
+    borderRadius: '50%',
+    padding: '5px',
+    height: '17px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    [theme.breakpoints.up('sm')]: {
+      left: '93px',
+    },
+  },
+  number: {
+    display: 'inline-block',
+    fontSize: fontSizeSmaller, // TODO: check correctness from rebase, was '12px',
+    fontWeight: fontWeightMedium,
+    marginBottom: '-3px',
+  },
 }));
 
-const TabNavigationAction = ({ className, ...props }) => {
+const TabNavigationAction = ({ className, itemsCounter, ...props }) => {
   const classes = useStyles();
 
   return (
-    <Tab
-      classes={{
-        root: clsx(className, classes.tab),
-        selected: classes.selected,
-      }}
-      {...props}
-    />
+    <Box className={classes.tabContainer}>
+      <Tab
+        classes={{
+          root: clsx(className, classes.tab),
+          selected: classes.selected,
+        }}
+        {...props}
+      ></Tab>
+      {itemsCounter && (
+        <Box className={classes.itemsCounterContainer}>
+          <span className={classes.number}>{itemsCounter}</span>
+        </Box>
+      )}
+    </Box>
   );
 };
 
 TabNavigationAction.propTypes = {
   className: PropTypes.string,
+  itemsCounter: PropTypes.number,
 };
 
 export default TabNavigationAction;
