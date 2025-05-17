485057d7887a99409dd44de3c4725ee92d085c65
mikozet <mikozet2@gmail.com>
2025-05-17 11:29:32 +0200

    Update info text

--- src/components/ShortMessage.js
+++ src/components/ShortMessage.js
@@ -9,13 +9,9 @@ import { DISCORD_URL, METRI_URL } from '~/utils/constants';
 const useStyles = makeStyles(() => ({
   messageContainer: {
     marginTop: '10px',
+    textAlign: 'center',
     '& p': {
       marginBottom: '3px',
-    },
-    textAlign: 'center',
-    '& a:first-child': {
-      marginBottom: '10px',
-      display: 'block',
     },
   },
 }));
@@ -28,7 +24,11 @@ const ShortMessage = () => {
       <Typography align="center" variant="body1">
         {translate('Login.ShortMsgText')}
       </Typography>
-      <ExternalLink href={DISCORD_URL} variant="body2">
+      <ExternalLink
+        href={METRI_URL}
+        variant="body2"
+        style={{ marginBottom: '10px', display: 'block' }}
+      >
         {METRI_URL}
       </ExternalLink>
       <Typography align="center" variant="body1">
